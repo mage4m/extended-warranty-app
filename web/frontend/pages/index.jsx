b640e0a71d14f70e84623dfe4f358bd69f418a02
saad-mage4 <saad@mage4.com>
2024-11-19 23:25:52 +0500

    HomePage work

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -1,93 +1,105 @@
 import {
-  Card,
-  Page,
-  Layout,
-  TextContainer,
-  Image,
-  Stack,
-  Link,
-  Text,
+    Page,
+    Layout,
+    Form,
+    Text,
+    Button,
+    TextField,
+    Select,
+    Box,
+    FormLayout,
 } from "@shopify/polaris";
 import { TitleBar } from "@shopify/app-bridge-react";
 import { useTranslation, Trans } from "react-i18next";
-
-import { trophyImage } from "../assets";
-
-import { ProductsCard } from "../components";
+import "../assets/css/custom.scss";
+import { useState, useCallback } from "react";
 
 export default function HomePage() {
-  const { t } = useTranslation();
-  return (
-    <Page narrowWidth>
-      <TitleBar title={t("HomePage.title")} primaryAction={null} />
-      <Layout>
-        <Layout.Section>
-          <Card sectioned>
-            <Stack
-              wrap={false}
-              spacing="extraTight"
-              distribution="trailing"
-              alignment="center"
-            >
-              <Stack.Item fill>
-                <TextContainer spacing="loose">
-                  <Text as="h2" variant="headingMd">
-                    {t("HomePage.heading")}
-                  </Text>
-                  <p>
-                    <Trans
-                      i18nKey="HomePage.yourAppIsReadyToExplore"
-                      components={{
-                        PolarisLink: (
-                          <Link url="https://polaris.shopify.com/" external />
-                        ),
-                        AdminApiLink: (
-                          <Link
-                            url="https://shopify.dev/api/admin-graphql"
-                            external
-                          />
-                        ),
-                        AppBridgeLink: (
-                          <Link
-                            url="https://shopify.dev/apps/tools/app-bridge"
-                            external
-                          />
-                        ),
-                      }}
+    // Variables
+    const [dashboard, setDashboard] = useState("Warranty Upsell");
+    const [typeOfUpSell, setTypeOfUpSell] = useState("");
+    const [policyName, setPolicyName] = useState("");
+    const [warrantyPrice, setWarrantyPrice] = useState(0);
+    const [selected, setSelected] = useState("Warranty");
+
+    // Translation
+    const { t } = useTranslation();
+
+    // Field Functions
+    const upSellChange = useCallback((e) => setTypeOfUpSell(e));
+    const policyChange = useCallback((e) => setPolicyName(e));
+    const warrantyPriceChange = useCallback((e) => setWarrantyPrice(e));
+    const handleSelectChange = useCallback((e) => {
+        if (e === "Warranty") {
+            setDashboard("Warranty Upsell");
+        } else {
+            setDashboard("Extended Warranty Upsell");
+        }
+        setSelected(e);
+    });
+
+    // Handle Submit
+    const handleSubmit = useCallback((e) => {}, []);
+
+    // Select Options
+    const options = [
+        { label: "Warranty", value: "Warranty" },
+        { label: "Extended Warranty", value: "Extended Warranty" },
+    ];
+
+    return (
+        <>
+            <div id="homePage">
+                <Page>
+                    <TitleBar
+                        title={t("HomePage.title")}
+                        primaryAction={null}
                     />
-                  </p>
-                  <p>{t("HomePage.startPopulatingYourApp")}</p>
-                  <p>
-                    <Trans
-                      i18nKey="HomePage.learnMore"
-                      components={{
-                        ShopifyTutorialLink: (
-                          <Link
-                            url="https://shopify.dev/apps/getting-started/add-functionality"
-                            external
-                          />
-                        ),
-                      }}
-                    />
-                  </p>
-                </TextContainer>
-              </Stack.Item>
-              <Stack.Item>
-                <div style={{ padding: "0 20px" }}>
-                  <Image
-                    source={trophyImage}
-                    alt={t("HomePage.trophyAltText")}
-                    width={120}
-                  />
-                </div>
-              </Stack.Item>
-            </Stack>
-          </Card>
-        </Layout.Section>
-        <Layout.Section>
-          <ProductsCard />
-        </Layout.Section>
-      </Layout>
-    </Page>
-  );
+                    <Layout>
+                        <Layout.Section>
+                            <Box id="title-Box">
+                                <Text variant="heading3xl" as="h2">
+                                    Create New {dashboard}
+                                </Text>
+                            </Box>
+                            <Form onSubmit={handleSubmit}>
+                                <FormLayout>
+                                    <FormLayout.Group>
+                                        <Select
+                                            label="Type of Upsell"
+                                            options={options}
+                                            onChange={handleSelectChange}
+                                            value={selected}
+                                        />
+                                        <TextField
+                                            label="Policy Name"
+                                            value={typeOfUpSell}
+                                            onChange={upSellChange}
+                                        />
+                                        <TextField
+                                            label="Duration (Years)"
+                                            value={policyName}
+                                            onChange={policyChange}
+                                        />
+                                        <TextField
+                                            label={
+                                                selected === "Warranty"
+                                                    ? "Warranty Price"
+                                                    : "Extended Warranty Price"
+                                            }
+                                            value={warrantyPrice}
+                                            onChange={warrantyPriceChange}
+                                        />
+                                    </FormLayout.Group>
+                                    <Button submit primary>
+                                        Submit
+                                    </Button>
+                                </FormLayout>
+                            </Form>
+                        </Layout.Section>
+                    </Layout>
+                </Page>
+            </div>
+        </>
+    );
 }
