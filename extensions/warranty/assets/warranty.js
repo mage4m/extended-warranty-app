92131d9db8cc0b84d4fb04737bd1264c03e33e2c
Taha Ahmed <taha@mage4.com>
2024-12-03 22:07:05 +0500

    cart work

--- extensions/warranty/assets/warranty.js
+++ extensions/warranty/assets/warranty.js
@@ -1,14 +1,23 @@
 document.addEventListener("alpine:init", () => {
     Alpine.data("warranty", () => ({
+        // productId: window.warrantyData.productId,
+        // customerId: window.warrantyData.customerId,
+        // shopDomain: window.warrantyData.shopDomain,
         appUrl: null,
         productId: null,
         customerId: null,
         shopDomain: null,
-        // productId: window.warrantyData.productId,
-        // customerId: window.warrantyData.customerId,
-        // shopDomain: window.warrantyData.shopDomain,
-
+        product: null,
         open: false,
+        warrantyOptions: [],
+        warrantyType: "",
+        clausesModal: false,
+        selectedClauses: [],
+        selectedWarrantyName: null,
+
+        // cart
+        cartLoader: false,
+        cart: [],
 
         init() {
             const warrantyContainer = document.querySelector(
@@ -20,6 +29,14 @@ document.addEventListener("alpine:init", () => {
                 warrantyContainer.getAttribute("data-customer-id");
             this.shopDomain =
                 warrantyContainer.getAttribute("data-shop-domain");
+            this.product = warrantyContainer.getAttribute("data-product");
+            console.log("FETCh", this.product);
+
+            fetch(window.Shopify.routes.root + this.product)
+                .then((response) => response.json())
+                .then((product) =>
+                    alert("The title of this product is " + product.title),
+                );
 
             //! Fetch data from the API
             fetch(
@@ -33,15 +50,105 @@ document.addEventListener("alpine:init", () => {
                     }
                     return response.json();
                 })
-                .then((data) => console.log(data))
+                .then((data) => {
+                    const warranties = data?.data ?? [];
+                    const enabledWarranties = warranties.filter(
+                        (item) => item?.status === "enabled",
+                    );
+                    this.open = enabledWarranties?.length > 0;
+                    console.log("TAHHAA", this.open);
+
+                    if (this.open) {
+                        // Populate warrantyOptions array with data
+                        this.warrantyOptions = enabledWarranties?.map(
+                            (item) => ({
+                                warrantyType: `${item?.duration_number} ${item?.duration_unit} ${item?.type} Available`,
+                                name: item?.name,
+                                duration: `${item?.duration_number} ${item?.duration_unit}`,
+                                price: `$${item?.price}`,
+                                clauses: item?.clauses,
+                            }),
+                        );
+
+                        // Set default values for the first warranty option
+                        const firstWarranty = this.warrantyOptions[0];
+                        this.selectedWarrantyName = firstWarranty.name;
+                        this.warrantyType = firstWarranty.warrantyType;
+                        this.selectedClauses = firstWarranty.clauses;
+                    }
+                })
                 .catch((error) =>
                     console.error("Error fetching data:", error?.message),
                 );
         },
 
-        toggle() {
-            alert("ataha");
-            this.open = !this.open;
+        selectWarranty(option) {
+            this.selectedWarrantyName = option.name;
+            this.warrantyType = option.warrantyType;
+            this.selectedClauses = option.clauses;
+        },
+
+        openModal() {
+            if (this.selectedClauses.length > 0) {
+                this.clausesModal = true;
+            }
+        },
+
+        closeModal() {
+            this.clausesModal = false;
+        },
+
+        async AddToCart() {
+            this.cartLoader = true;
+            try {
+                const { cart } = this;
+                const cartItems = cart?.items || [];
+                let updates = {};
+
+                // Handling the product ID if it's provided as an array
+                if (
+                    Array.isArray(this.productId) &&
+                    this.productId.length > 0
+                ) {
+                    const currentItem = cartItems.find(
+                        (item) => item?.variant_id === this.productId[0],
+                    );
+                    updates[this.productId] = currentItem
+                        ? currentItem.quantity
+                        : 1;
+                }
+
+                const response = await fetch(
+                    // `${this.appUrl}/cart/update.js`,
+                    window.Shopify.routes.root + "cart/update.js",
+                    {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({ updates }),
+                    },
+                );
+
+                // Check if the response is okay (status 200-299)
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+
+                // Parse the response as JSON
+                const data = await response.json();
+
+                // Update the cart with the response data
+                cart = data || [];
+            } catch (error) {
+                if (error?.response?.data?.status === 422) {
+                    console.log(error?.response?.data?.description);
+                } else {
+                    console.error(`${error?.message}`, "error");
+                }
+            } finally {
+                this.cartLoader = false;
+            }
         },
     }));
 });
